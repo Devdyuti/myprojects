411d6c2fba5538b6780c1bad2e08e1ab1b125e23
Devdyuti <singhking5414@hotmail.com>
2019-06-04 21:58:08 +0530

    clear repo data

--- Tutorial/src/org/divye/design_pattern/abstract_factoryPattern/Color.java
+++
@@ -1,5 +0,0 @@
-package org.divye.design_pattern.abstract_factoryPattern;
-
-public interface Color {
-	abstract void fill();
-}
--- Tutorial/src/org/divye/design_pattern/front_controllor/StudentView.java
+++
@@ -1,7 +0,0 @@
-package org.divye.design_pattern.front_controllor;
-
-public class StudentView {
-	public void show(){
-		System.out.println("Display Student page");
-	}
-}
