1557d50b1e480b3d2787fc060c836ab26b84b0a4
dsingh <devdyuti.singh@soprasteria.com>
2019-06-05 19:44:32 +0530

    updated jpa project [Src & pom] from workplace

---
+++ tutorial-project/spring-boot-jpa-tuto/src/main/java/org/dev/tu/sbj/tuto/entity/Dog.java
@@ -0,0 +1,34 @@
+package org.dev.tu.sbj.tuto.entity;
+
+public class Dog {
+	
+/*	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)*/
+	private Long id;
+	private String name;
+	private int age;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- tutorial-project/spring-boot-jpa-tuto/src/main/java/org/spring/boot/jpa/tuto/entity/Dog.java
+++
@@ -1,5 +0,0 @@
-package org.spring.boot.jpa.tuto.entity;
-
-public class Dog {
-
-}
