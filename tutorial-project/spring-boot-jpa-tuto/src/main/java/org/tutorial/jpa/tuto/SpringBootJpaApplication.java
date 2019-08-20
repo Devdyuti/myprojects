078f04e26026d706e5447eb7747df2f243cb627b
Devdyuti <singhking5414@hotmail.com>
2019-08-20 11:32:52 +0530

    updated SpringBootJpaApplication.java file

--- tutorial-project/spring-boot-jpa-tuto/src/main/java/org/tutorial/jpa/tuto/SpringBootJpaApplication.java
+++ tutorial-project/spring-boot-jpa-tuto/src/main/java/org/tutorial/jpa/tuto/SpringBootJpaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories(basePackages="org.tutorial.jpa.tuto.repository")
 @SpringBootApplication
-@ComponentScan(basePackages="org.tutorial.jpa.tuto")
+@ComponentScan(basePackages="org.tutorial.jpa.tutoX")
 public class SpringBootJpaApplication {
 	
 	public static void main(String[] args) {
