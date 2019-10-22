29c661e2d0c56f19587c863a08bf98f682c6a981
Devdyuti <singhking5414@hotmail.com>
2019-10-22 17:03:31 +0530

    latest code

--- tutorial-project/spring-mvc-tuto/src/main/java/org/tutorial/spring/mvc/tuto/config/EmployeeConfig.java
+++ tutorial-project/spring-mvc-tuto/src/main/java/org/tutorial/spring/mvc/tuto/config/EmployeeConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -18,6 +20,18 @@ public class EmployeeConfig extends WebMvcConfigurationSupport{
 		jspViewResolver.setSuffix(".jsp");
 		jspViewResolver.setViewClass(JstlView.class);
 		return jspViewResolver;
+	}
+	
+	@Bean
+	public DefaultAnnotationHandlerMapping defaultHandlerMapping() {
+		DefaultAnnotationHandlerMapping handler=new DefaultAnnotationHandlerMapping();
+		return handler;
+	}
+	
+	@Bean
+	public AnnotationMethodHandlerAdapter annotationHandlerMapping() {
+		AnnotationMethodHandlerAdapter handler=new AnnotationMethodHandlerAdapter();
+		return handler;
 	}
 	
 	@Override
