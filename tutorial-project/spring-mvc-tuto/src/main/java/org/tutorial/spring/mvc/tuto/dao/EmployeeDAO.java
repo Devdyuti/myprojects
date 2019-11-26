08080bb4e0ea4ba65d6cc96e686043aa04e434bc
Devdyuti <singhking5414@hotmail.com>
2019-11-26 18:18:47 +0530

    deleted the spring-mvc-tuto from the tutorial-project

--- tutorial-project/spring-mvc-tuto/src/main/java/org/tutorial/spring/mvc/tuto/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package org.tutorial.spring.mvc.tuto.dao;
-
-import java.util.List;
-
-import org.tutorial.spring.mvc.tuto.model.Employee;
-
-public interface EmployeeDAO {
-	public List<Employee> getAllEmployees();
-}
--- tutorial-project/spring-mvc-tuto/src/main/java/org/tutorial/spring/mvc/tuto/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package org.tutorial.spring.mvc.tuto.service;
-
-import java.util.List;
-
-import org.tutorial.spring.mvc.tuto.model.Employee;
-
-public interface EmployeeService {
-	public List<Employee> getAllEmployees();
-}
